2c21e685fc83b0feeffcc3cfb56d4fa58fb51788
Erwin Mardinata <537.mardinata@gmail.com>
2016-02-12 11:43:50 +0800

    website sederhana program studi menggunaka codeigniter

---
+++ application/views/front/template/footer.php
@@ -0,0 +1,28 @@
+<footer>
+	<div class="col-lg-3 col-md-3">
+    	<h4>Tentang TI-UTS</h4>
+            <p><a href="#">Sejarah</a></p>
+            <p><a href="#">Visi dan Misi</a></p>
+            <p><a href="#">Slogan</a></p>
+    </div>
+    <div class="col-lg-3 col-md-3">
+    	<h4>Kontak</h4>
+            <p><span class="glyphicon glyphicon-home"></span> Jl. Raya Olat Maras, Batu Alang,
+Moyo Hulu, Kab. Sumbawa Besar</p>
+            <p><span class="glyphicon glyphicon-phone"></span> 087863577415</p>
+            <p><span class="glyphicon glyphicon-envelope"></span> 537.mardinata@gmail.com</p>
+    </div>
+    <div class="col-lg-3 col-md-3">
+    	<h4>Kerja Sama</h4>
+             <a href="#"><img src="<?php echo base_url().'assets/bootstrap/images/utss.png'; ?>" /></a>&nbsp;
+             <a href="#"><img src="<?php echo base_url().'assets/bootstrap/images/utss.png'; ?>" /></a>&nbsp;
+             <a href="#"><img src="<?php echo base_url().'assets/bootstrap/images/utss.png'; ?>" /></a>
+    </div>
+    <div class="col-lg-3 col-md-3">
+        <h4>Media Sosial</h4>
+             <a href="https://www.facebook.com/ti.utsumbawa" target="_blank"><img src="<?php echo base_url().'assets/bootstrap/images/facebook.png'; ?>" /></a>&nbsp;
+             <a href="http://www.twitter.com/ErwinMardinata" target="_blank"><img src="<?php echo base_url().'assets/bootstrap/images/twitter.png'; ?>" /></a>
+    </div>
+    <div style="clear: both"></div><hr>
+<p align="center">&copy; <a href="http://facebook.com/erwinanakcbw" target="_blank">Erwin Mardinata</a> 2015</p></br>
+</footer>
